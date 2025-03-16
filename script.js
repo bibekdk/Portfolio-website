0e619079c5c97bf1d2e1d939fe74734b2a645aab
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-03-16 17:57:54 +0545

    Updated Bio

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onscroll = () => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribte("id");
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
       navLinks.forEach((links) => {
